7f8df4fe429cc971772b55d3adaf3e7eaffc2d3a
ammerss <amyss0907@gmail.com>
2022-05-05 15:35:46 -0400

    ee

--- lab4_assign/main.cpp
+++ lab4_assign/main.cpp
@@ -33,7 +33,6 @@ int main(int argc, char *argv[]){
 		if(line[0] == '#' || line.length() == 0)continue;
 		int time_step, track;
 		sscanf(line.c_str(),"%d %d", &time_step, &track);
-		//cout << time_step << track << endl;
 		struct IO temp = {time_step, track, 0, 0};
 		io_list.push_back(temp);
 	}
@@ -47,7 +46,7 @@ int main(int argc, char *argv[]){
 	double avg_turnaround=0;
 	double avg_waittime=0;
 	int max_waittime=0;
-	//for(int cnt=0;cnt<io_list.size();cnt++)
+
 	while(1){
 		while(cnt <= io_list.size()-1 && io_list[cnt].time_step <= time){
 			sched->add_io(&io_list[cnt]);
@@ -67,12 +66,8 @@ int main(int argc, char *argv[]){
 				max_waittime = max(max_waittime, wait);
 				sched->set_dir(active->track, cur_track, dir);
 				if(active->track == cur_track)continue;
-				//cout << "starting to search for " <<active->track<<endl;
 			}
 			if(active==NULL && sched->empty() && cnt>=io_list.size())break;
-			//if(active==NULL && cnt < io_list.size())continue;
-			//active->start_time = time;
-			//sched->set_dir(active->track, cur_track, dir);
 		}	
 		if(active){
 			if(dir==1) cur_track++;
@@ -80,7 +75,6 @@ int main(int argc, char *argv[]){
 			total_movement++;
 		}
 		time++;
-		//cout << time <<" "<< cur_track << endl;
 	
 	}
 	for(int i=0;i<io_list.size();i++){
